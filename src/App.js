50aec747711cdbb2403038752858708d528f7bec
shokosama <davit.buchukuri19b@geolab.edu.ge>
2021-07-21 00:08:08 +0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Questions from './components/questions';
+import DropDownDifficulty from './components/dropDownDifficulty';
+import Score from './components/score';
+import {useState, createContext} from 'react'
+import axios from 'axios';
+import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom'
 function App() {
+  
+  const getdData = (values)=>{
+    sessionStorage.setItem('category',`${values[0]}`)
+    sessionStorage.setItem('difficulty', `${values[1]}`)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Route exact path='/'>
+          <DropDownDifficulty sendData={getdData}/>
+        </Route>
+          <Route exact path = '/quiz'>
+            <Questions/>
+          </Route>
+          <Route exact path = '/score'>
+            <Score/>
+          </Route>
+      </Router>
     </div>
   );
 }
